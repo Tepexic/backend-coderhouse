f2bedd2063b2b6ba11d07bcdcdf7b341e321f322
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-20 18:27:01 -0500

    clase 11

---
+++ Clase11/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+// Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
+app.use(express.static("./public"));
+// Esta ruta carga nuestro archivo index.html en la raíz de la misma
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+// El servidor funcionando en el puerto 3000
+httpServer.listen(3000, () => console.log("SERVER ON"));
+
+io.on("connection", (socket) => {
+  // "connection" se ejecuta la primera vez que se abre una nueva conexión
+  console.log("Usuario conectado");
+  // Se imprimirá solo la primera vez que se ha abierto la conexión
+  socket.emit("mensaje", "mensaje del servidor al cliente"); // (evento, datos)
+});
