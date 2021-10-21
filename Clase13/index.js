bf067ab37f5742f03059937838c60dd0cd452f84
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-21 10:07:33 -0500

    clase 13

---
+++ Clase13/index.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+*Realizar un programa que genere un color aleatorio en formato RGB (canal rojo, verde y azul entre 0 y 255) y lo muestre por consola. Este estará implementado en un archivo llamado color.js
+La funcionalidad debe estar implementada dentro de una clase y deberá utilizar sintaxis ES6 (const, let, arrow function y template string).
+Convertir este código ES6 a JS5 con Babel online. Realizar esta conversión en forma automática dentro de un proyecto node.js que utilice Babel CLI
+
+tsc -w
+*/
+function randomColor() {
+    return Math.round(Math.random() * 255);
+}
+function rgb() {
+    return [randomColor(), randomColor(), randomColor()];
+}
+console.log(rgb);
